4482b7fe97d11538f9d82ebde3b95fdbd77f9605
Judy <judyjoiel@gmail.com>
2025-02-17 16:57:23 +0100

    server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,20 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
 import recipeRoutes from "./routes/recipeRoutes.js";
 
-dotenv.config();
-const app = express();
+dotenv.config();  
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipeRoutes);
 
-
-dotenv.config();
-
 mongoose
   .connect(process.env.MONGO_URI) 
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
