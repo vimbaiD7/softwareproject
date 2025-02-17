44273bffd2ca8829e97dfb5b6037c4167b7c739c
Judy <judyjoiel@gmail.com>
2025-02-17 16:58:00 +0100

    authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,17 +4,17 @@ const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
   
     if (!token) {
-      console.log("❌ No token provided.");
+      console.log("No token provided.");
       return res.status(401).json({ error: "Unauthorized" });
     }
   
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log("✅ Authenticated User ID:", decoded.id); 
+      console.log("Authenticated User ID:", decoded.id); 
       req.user = decoded;
       next();
     } catch (error) {
-      console.log("❌ Invalid token:", error);
+      console.log("Invalid token:", error);
       return res.status(403).json({ error: "Invalid token" });
     }
   };
