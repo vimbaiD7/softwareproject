a19d66093c7d71183a9d3c20444add6a4b269e3a
Judy <judyjoiel@gmail.com>
2025-02-17 16:59:49 +0100

    recipeApi.js

--- backend/src/recipeApi.js
+++ backend/src/recipeApi.js
@@ -6,7 +6,7 @@ export const fetchMyRecipes = async () => {
   try {
     const token = localStorage.getItem("token");
     if (!token) {
-      console.error("❌ No token found. User is not logged in.");
+      console.error("No token found. User is not logged in.");
       return [];
     }
 
@@ -14,10 +14,10 @@ export const fetchMyRecipes = async () => {
       headers: { Authorization: `Bearer ${token}` }, 
     });
 
-    console.log("✅ Fetched user recipes:", response.data); 
+    console.log("Fetched user recipes:", response.data); 
     return response.data;
   } catch (error) {
-    console.error("❌ Error fetching my recipes:", error);
+    console.error("Error fetching my recipes:", error);
     return [];
   }
 };
